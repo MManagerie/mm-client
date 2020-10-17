5af84d9cc272601b7a310073410a78f5351b9eea
idhoramgg <31439757+idhoramgg@users.noreply.github.com>
2020-10-17 11:01:06 +0700

    Update axios.js

--- src/helpers/axios.js
+++ src/helpers/axios.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 let token = localStorage.getItem('menagerie');
 
 // axios.defaults.baseURL = 'http://localhost:8000'
-axios.defaults.baseURL = 'https://mm-ser.herokuapp.com/api/api/'
+axios.defaults.baseURL = 'https://mm-ser.herokuapp.com/api/'
 axios.defaults.headers.common = {'Authorization': `Bearer ${token}`}
 
 export default axios;
